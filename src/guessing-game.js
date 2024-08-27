6f1a4cd7a60cb00b505f9ac8cdb3982ca86c3f9d
MaryanaKarunets <137786526+MaryanaKarunets@users.noreply.github.com>
2024-08-28 00:13:58 +0300

    add:
    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
-
+    cconstructor() {
+        let a;
+        let b; 
+        let c;
+    }
+    
     setRange(min, max) {
-
+      this.a = min;
+      this.b = max;
     }
 
     guess() {
-
+      this.c = Math.ceil((this.a + this.b)/2);
+      return this.c;
     }
 
     lower() {
-
+       this.b = Math.ceil(this.c);
     }
 
     greater() {
-
+        this.a = Math.ceil(this.c);
     }
 }
 
